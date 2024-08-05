1d7a62aeba7c30edb88860708d9943b63bea2289
Roman Menzheres <roma.menzheres@gmail.com>
2024-08-05 20:44:21 +0300

    [FIX] Changed @NotNull to @NotBlank in CreateCustomerRequestDto

--- src/main/java/com/example/task/dto/customer/CreateCustomerRequestDto.java
+++ src/main/java/com/example/task/dto/customer/CreateCustomerRequestDto.java
@@ -1,15 +1,15 @@
 package com.example.task.dto.customer;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 public record CreateCustomerRequestDto(
-        @NotNull
+        @NotBlank
         @Length(min = 2, max = 50)
         String fullName,
-        @NotNull
+        @NotBlank
         @Length(min = 2, max = 100)
         @Email
         String email,
